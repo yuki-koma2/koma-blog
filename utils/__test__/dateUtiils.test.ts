0c7175f6b952f6bfb5f5bd163be906b87a740b1f
yukikoma <yuki.k.lion@gmail.com>
2022-09-27 09:58:50 +0900

    sample test code .

--- utils/__test__/dateUtiils.test.ts
+++ utils/__test__/dateUtiils.test.ts
@@ -1,4 +1,4 @@
-import { getDaysSinceDate } from "../dateUtils";
+import { calculateDaysBetweenDates, getDaysSinceDate } from "../dateUtils";
 
 describe('date util test', function () {
     describe('#getDaysSinceDate', function () {
@@ -19,5 +19,27 @@ describe('date util test', function () {
                 const days = getDaysSinceDate(date);
                 expect(days).toBe(2);
             });
+    });
+    describe('#calculateDaysBetweenDates', function () {
+        it('should return 0 if dates are the same', function () {
+            const date1 = new Date();
+            const date2 = new Date();
+            const days = calculateDaysBetweenDates(date1, date2);
+            expect(days).toBe(0);
+        });
+        it('should return 1 if dates are 1 day apart', function () {
+            const date1 = new Date();
+            const date2 = new Date();
+            date2.setDate(date2.getDate() + 1);
+            const days = calculateDaysBetweenDates(date1, date2);
+            expect(days).toBe(1);
+        });
+        it('should return 2 if dates are 2 days apart', function () {
+            const date1 = new Date();
+            const date2 = new Date();
+            date2.setDate(date2.getDate() + 2);
+            const days = calculateDaysBetweenDates(date1, date2);
+            expect(days).toBe(2);
+        });
     });
 });

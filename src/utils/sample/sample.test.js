5ea418a6d24114c746e13683ca5a8a36d24ef90d
yukikoma <yuki.k.lion@gmail.com>
2023-01-08 15:01:54 +0900

    フィボナッチ数列の例題に挑戦中。

---
+++ src/utils/sample/sample.test.js
@@ -0,0 +1,25 @@
+import { yourself } from "./sample";
+
+describe('sample code test', function () {
+    it('should return 1', function () {
+        expect(1).toBe(1)
+    });
+    it('should return 0', function () {
+        expect(yourself.fibonacci(0)).toBe(0)
+    });
+    it('should return 1', function () {
+        expect(yourself.fibonacci(1)).toBe(1)
+    });
+    it('should return 1', function () {
+        expect(yourself.fibonacci(2)).toBe(1)
+    });
+    it('should return 2', function () {
+        expect(yourself.fibonacci(3)).toBe(2)
+    });
+    it('should return 3', function () {
+        expect(yourself.fibonacci(4)).toBe(3)
+    });
+    it('should return 5', function () {
+        expect(yourself.fibonacci(5)).toBe(5)
+    });
+});

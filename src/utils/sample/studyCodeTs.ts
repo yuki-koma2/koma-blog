eb7e119085abadfe9b7cc6060cf6c222ae08ab5d
yukikoma <yuki.k.lion@gmail.com>
2023-01-08 15:44:32 +0900

    refactor

--- src/utils/sample/studyCodeTs.ts
+++ src/utils/sample/studyCodeTs.ts
@@ -13,35 +13,30 @@ export const romanToInt = (s: string): number => {
     for (let i = 0; i < s.length; i++) {
         const roman = <RomanString>s[i]
 
-        // CM,XC,IVの例外ケースへの対応
-        // if (roman === 'C' || roman === 'X' || roman === 'I') {
+        // CM,XC,IVの例外ケースへの対応 パフォーマンス考慮するほどではなさそう。
         const nextRoman = <RomanString>s[i + 1]
         const twoRoman = roman + nextRoman;
-        if (twoRoman === 'CM') {
-            calc = calc + 900
-            i++
-        }
-        else if (twoRoman === 'XC') {
-            calc = calc + 90
-            i++
-        }
-        else if (twoRoman === 'IV') {
-            calc = calc + 4
-            i++
-        } else {
-            const convertedNumber = romanNumberMap.get(roman) ?? 0;
-            calc = calc + convertedNumber;
-        }
-        // }
+        switch (twoRoman) {
+            case 'CM':
+                calc = calc + 900
+                i++
+                break;
+            case 'XC':
+                calc = calc + 90
+                i++
+                break;
+            case 'IV':
+                calc = calc + 4
+                i++
+                break;
+            default:
+                const convertedNumber = romanNumberMap.get(roman) ?? 0;
+                calc = calc + convertedNumber;
 
 
+        }
     }
     return calc
 };
 
 type RomanString = 'I' | 'V' | 'X' | 'L' | 'C' | 'D' | 'M';
-
-
-// type RomanNumberMap = {
-//     [k in RomanString]: number;
-// };

0aba668f1bf3c9f6dfe336456cc6508a3f9e4a26
yukikoma <yuki.k.lion@gmail.com>
2023-02-26 17:56:15 +0900

    fix: Mantine の影響範囲を削除

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,27 +1,7 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
-import { createGetInitialProps } from '@mantine/next';
 import { CssBaseline } from "@nextui-org/react";
 
-// const stylesServer = createStylesServer();
-const getInitialProps = createGetInitialProps();
-
 export default class _Document extends Document {
-
-    static getInitialProps = getInitialProps;
-    // static async getInitialProps(ctx: DocumentContext) {
-    //     const initialProps = await Document.getInitialProps(ctx);
-    //
-    //     // Add your app specific logic here
-    //
-    //     return {
-    //         ...initialProps,
-    //         styles: [
-    //             initialProps.styles,
-    //             <ServerStyles html={initialProps.html} server={stylesServer} key="styles" />,
-    //         ],
-    //     };
-    // }
-
     // NOTE Head にタイトルは書かない。
     render() {
         return (

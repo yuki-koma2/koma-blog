fb82d2bdf36f52ee6c25c6b05e0caa0f55ad3959
yukikoma <yuki.k.lion@gmail.com>
2023-03-04 18:45:09 +0900

    feat: add Hover card component

--- src/components/atoms/card/HoverCard.tsx
+++ src/components/atoms/card/HoverCard.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import styleModule from './HoverCard.module.scss';
+// NOTE : This is a sample component.
+// TODO: カードの枠にうっすらグラデーションのボーダーをつけようとしたが、この方法だとうまくいかなそうなので気が向いたらちゃんと作る
 export default function HoverCard() {
     return (
-        <div className={styleModule.card}>
-            Magic Card
+        <div className={styleModule.container}>
+            <div className={styleModule.card}>
+                Card
+            </div>
         </div>
     );
 }
